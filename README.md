4c235accb909bc616e1b06ee030bbce1b4bb2f55
Twinkling <twinkling8023@gmail.com>
2022-06-04 17:54:39 +0800

    init: init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue 3 + TypeScript + Vite
+
+A Vue3 project boilerplate with ts, vite, eslint, stylelint, prettier, husky, lint-staged, commitlint, GitHub pages actions support.
+
+## Getting Started
---
+++ src/views/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png" alt="" />
+    <h1>Vite + Vue3 + TypeScript</h1>
+    <div class="content">
+      <h2>Getting Started</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "home",
+  components: {},
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.home {
+}
+.content {
+  text-align: left;
+  padding: 0 16px;
+}
+</style>
